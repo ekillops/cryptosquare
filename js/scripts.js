39f68200beae405988dc410e72fb9fb3921c4083
Erik Killops and Steve Burton <erik@>
2016-10-27 15:18:23 -0700

    add javascript to determine string length and square root, rounds up to get grid size

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,14 @@
+//Strips punctuation, white space and makes text lowercase.
 var inputPrep = function(inputString) {
   var processString = inputString.replace(/[.,?!'"\s]/g,"").toLowerCase();
   return processString;
 };
 
+//Calculate length of string and determins nearest square root rounding up to get grid size.
+var nearSquare = function(stringSize) {
+  var gridSize = Math.ceil(Math.sqrt(stringSize.length));
+  return gridSize;
+};
 
 
 $(document).ready(function(){
